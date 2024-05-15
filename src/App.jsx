976705c835146cb27ed2c204ba0c96f84620290a
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-05-15 12:26:28 -0400

    Add edit button

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,66 @@ function App() {
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
+
+  const [submittedFirstName, setSubmittedFirstName] = useState('')
+  const [submittedLastName, setSubmittedLastName] = useState('')
+  const [submittedEmail, setSubmittedEmail] = useState('')
+  const [submittedPhoneNumber, setSubmittedPhoneNumber] = useState('')
+
   const [submitted, setSubmmited] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault()
     setSubmmited(true)
+    setIsEditing(false)
+
+    setSubmittedFirstName(firstName)
+    setSubmittedLastName(lastName)
+    setSubmittedEmail(email)
+    setSubmittedPhoneNumber(phoneNumber)
+
+    setFirstName('')
+    setLastName('')
+    setEmail('')
+    setPhoneNumber('')
+  }
+
+  const handleEdit = () => {
+    setIsEditing(true)
+    setFirstName(submittedFirstName)
+    setLastName(submittedLastName)
+    setEmail(submittedEmail)
+    setPhoneNumber(submittedPhoneNumber)
   }
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <h1>Personal Information</h1>
-        <General
-          firstName={firstName}
-          setFirstName={setFirstName}
-          lastName={lastName}
-          setLastName={setLastName}
-          email={email}
-          setEmail={setEmail}
-          phoneNumber={phoneNumber}
-          setPhoneNumber={setPhoneNumber}
-        />
-        <button type='submit'>Submit</button>
-      </form>
-      {submitted && (
+      {!submitted || isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <h1>Personal Information</h1>
+          <General
+            firstName={firstName}
+            setFirstName={setFirstName}
+            lastName={lastName}
+            setLastName={setLastName}
+            email={email}
+            setEmail={setEmail}
+            phoneNumber={phoneNumber}
+            setPhoneNumber={setPhoneNumber}
+          />
+          <div className='submit-button'>
+            <button type='submit'>Submit</button>
+          </div>
+        </form>
+      ) : (
         <div>
-          <p>First Name: {firstName}</p>
-          <p>Last Name: {lastName}</p>
-          <p>Email: {email}</p>
-          <p>Phone Number: {phoneNumber}</p>
+          <h1>Personal Information</h1>
+          <p>First Name: {submittedFirstName}</p>
+          <p>Last Name: {submittedLastName}</p>
+          <p>Email: {submittedEmail}</p>
+          <p>Phone Number: {submittedPhoneNumber}</p>
+          <button onClick={handleEdit}>Edit</button>
         </div>
       )}
     </>
--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,3 +1,5 @@
+import '../styles/General.css'
+
 function General({
   firstName,
   setFirstName,
@@ -10,38 +12,40 @@ function General({
 }) {
   return (
     <>
-      <label>
-        First Name:{' '}
-        <input
-          type='text'
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-      </label>
-      <label>
-        Last Name:{' '}
-        <input
-          type='text'
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-      </label>
-      <label>
-        Email:{' '}
-        <input
-          type='email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label>
-        Phone Number:{' '}
-        <input
-          type='tel'
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-        />
-      </label>
+      <div className='general'>
+        <label>
+          First Name:{' '}
+          <input
+            type='text'
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </label>
+        <label>
+          Last Name:{' '}
+          <input
+            type='text'
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </label>
+        <label>
+          Email:{' '}
+          <input
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Phone Number:{' '}
+          <input
+            type='tel'
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+        </label>
+      </div>
     </>
   )
 }
